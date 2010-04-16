308dc8be5e5d2aaf06d650baeca1842dcf8f88bb
MikePK <mikepk@tenzerolab.com>
2010-04-16 11:35:58 -0400

    Adding an ErrorMiddleware WSGI component to handle errors separately from the Router and Application code.

---
+++ pybald/core/ErrorMiddleware.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+ErrorMiddleware.py
+
+Created by mikepk on 2010-04-14.
+Copyright (c) 2010 Michael Kowalchik. All rights reserved.
+"""
+
+import sys
+import os
+import unittest
+
+import datetime
+
+from webob import Request, Response, exc
+from mako import exceptions
+
+class ErrorMiddleware:
+    '''Code to handle errors, implemented as WSGI middleware.'''
+    def __init__(self,application=None,error_controller=None):
+        if application:
+            self.application = application
+        else:
+            # no pipeline so just generate a generic response
+            self.applicaion = Response()
+        self.error_controller = error_controller
+
+    def __call__(self,environ,start_response):
+        req = Request(environ)
+        #pass through if no exceptions occur
+        try:
+            resp = req.get_response(self.application)
+            return resp(environ,start_response)
+        # handle HTTP errors
+        except exc.HTTPError, err:
+            if self.error_controller:
+                try:
+                    controller = self.error_controller()
+                    action = self.error_controller.error_map[err.code]
+                    handler = getattr(controller,action)
+                    return handler(environ,start_response)
+                except (KeyError, AttributeError):
+                    handler = controller
+                    return handler(environ,start_response)
+                except:
+                    return err(environ,start_response)
+            else:
+                return err(environ,start_response)
+                
+        except Exception, err:
+            if self.error_controller:
+                controller = self.error_controller()
+                handler = controller
+                controller.message=str(err)
+                return handler(environ,start_response)
+            else:
+                # create a generic HTTP server Error webob exception
+                return exc.HTTPServerError()(environ,start_response)
+        
+
+class ErrorMiddlewareTests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
