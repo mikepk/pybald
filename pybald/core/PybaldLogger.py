b2b409ea792961806bb75cf2e49c6b7522580871
MikePK <mikepk@tenzerolab.com>
2010-04-16 15:27:15 -0400

    Actually make the logger work and act as WSGI middleware

--- pybald/core/PybaldLogger.py
+++ pybald/core/PybaldLogger.py
@@ -11,12 +11,18 @@ import sys
 import os
 import unittest
 
+from webob import Request, Response
+
 import logging
 import logging.handlers        
 
 class PybaldLogger:
-    def __init__(self,log_file = '/tmp/pybald_log.out'):
-        
+    def __init__(self,application=None,log_file = '/tmp/pybald.log'):
+        if application:
+            self.application = application
+        else:
+            # no pipeline so just generate a generic response
+            self.applicaion = Response()
         LOG_FILENAME = log_file
 
         # Set up a specific logger with our desired output level
@@ -25,7 +31,9 @@ class PybaldLogger:
 
         # Add the log message handler to the logger
         handler = logging.handlers.RotatingFileHandler(
-                      LOG_FILENAME, maxBytes=1024*1024*20, backupCount=50)
+                      LOG_FILENAME, 
+                      maxBytes=1024*1024*20, 
+                      backupCount=10)
 
         # create formatter
         formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
@@ -33,6 +41,15 @@ class PybaldLogger:
         handler.setFormatter(formatter)
 
         self.my_logger.addHandler(handler)
+        
+
+    def __call__(self,environ,start_response):
+        req = Request(environ)
+        sys.stdout = self
+        #environ['wsgi.errors'] =
+        #pass through if no exceptions occur
+        resp = req.get_response(self.application)
+        return resp(environ,start_response)
 
     def write(self,msg):
         if msg == '\n':
