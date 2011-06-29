e023049f53ba180f7516f1e0cb7bc93ae56c5751
David <davids@smarterer.com>
2011-06-29 15:26:39 -0400

    UserManager now continuously checks to see if the user has a valid() function, and if it does (and that function returns False) it ends the session.
    This isn't great, but for now it works reliably...

--- pybald/core/users.py
+++ pybald/core/users.py
@@ -33,10 +33,26 @@ class UserManager(object):
         else:
             environ['REMOTE_USER'] = None
 
+        if environ['REMOTE_USER']:
+            # Continuously validate user sessions
+            # TODO: Clean up our app-level login code a lot...
+
+            # Look for a "valid" method on User
+            try:
+                valid = getattr(environ['REMOTE_USER'], "valid")
+            except AttributeError:
+                # (If this method isn't defined, do nothing.)
+                pass
+            else:
+                # If available, call it, and expect a Boolean:
+                # If False, end the session right now.
+                if not valid():
+                    environ['REMOTE_USER'] = None
+                # Otherwise, do nothing
+
         # update or create the pybald.extension to populate controller instances
         environ['pybald.extension'] = environ.get('pybald.extension', {})
         environ['pybald.extension']['user'] = environ['REMOTE_USER']
-
 
         # call the next part of the pipeline
         resp = req.get_response(self.application)
