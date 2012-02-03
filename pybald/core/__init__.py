1cce9206c5b447ba37afe2d3ea0769958665932b
mikepk <mikepk@tenzerolab.com>
2012-02-03 11:13:13 -0500

    Better error messages when the automatic class loader fails to import a file

--- pybald/core/__init__.py
+++ pybald/core/__init__.py
@@ -8,6 +8,9 @@
 import os
 import re
 
+class PybaldImportError(Exception):
+    pass
+
 modname_pattern = re.compile(r'^([a-z]+[a-z_]*)\.py$', re.I)
 # TODO, use walk to have this recursively walk up the models path
 # finding all interesting classes.
@@ -18,7 +21,16 @@ def pybald_class_loader(path, classes, module_globals, module_locals, recursive=
             match = modname_pattern.search(filename)
             if match:
                 imp_modname = match.group(1)
-                model_module = __import__(imp_modname, module_globals, module_locals, [], 1)
+                # maybe pass on PybaldImportError?
+                try:
+                    model_module = __import__(imp_modname, module_globals, module_locals, [], 1)
+                except ImportError, ie:
+                    raise PybaldImportError("\nThe automatic pybald class loader "
+                    "failed while attempting to load the "
+                    "module {0} from {1}\n"
+                    "The most likely cause is a non pybald model or class in "
+                    "the app/models or app/controllers paths.\n"
+                    "Orignal message: {2}".format(filename, dirpath, ie))
                 for classname in dir(model_module):
                     if classname in loaded_classes:
                         continue
@@ -31,4 +43,6 @@ def pybald_class_loader(path, classes, module_globals, module_locals, recursive=
                     if pybald_model:
                         module_globals[classname] = module_class
                         loaded_classes.append(classname)
+        if not recursive:
+            break
     return loaded_classes
