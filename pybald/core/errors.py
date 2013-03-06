bf74e794d426e4234833d5b20c3e82a04204e5d4
mikepk <mikepk@smarterer.com>
2013-03-06 16:36:17 -0500

    Cleanup error handling, more docs

--- pybald/core/errors.py
+++ pybald/core/errors.py
@@ -8,9 +8,20 @@ console = logging.getLogger(__name__)
 
 class ErrorMiddleware:
     '''
-    Handles web exceptions
+    Handles exceptions during web transactions.
 
-    Implemented as WSGI middleware.
+    Implemented as WSGI middleware, this middleware wraps the application and
+    intercepts errors and exceptions. The middleware makes a distinction between
+    webob HTTP exceptions and other exception types.
+
+
+        :param application:  WSGI application/middleware that is to be
+                             *wrapped* by the error handler in the web app pipeline.
+
+        :param error_controller:  A class that is used by the middleware to handle
+                              errors for the project. When configured, an error_controller
+                              instance is created and the WSGI response is passed to
+                              the error_controller to handle.
     '''
     def __init__(self, application=None, error_controller=None):
         if application:
@@ -21,80 +32,35 @@ class ErrorMiddleware:
         self.error_controller = error_controller
 
     def __call__(self, environ, start_response):
-        req = Request(environ)
         #pass through if no exceptions occur
         try:
-            return req.get_response(self.application)(environ, start_response)
+            return self.application(environ, start_response)
         # handle HTTP errors
         except exc.HTTPException, err:
             # if the middleware is configured with an error controller
             # use that to display the errors
             console.debug("HTTP Exception thrown {0}".format(err))
             if self.error_controller:
-                try:
-                    handler = self.error_controller(status_code=err.code)
-                    # controller.status_code = err.code
-                    # action = self.error_controller.error_map[err.code]
-                    # handler = controller #getattr(controller, action, None) or err
-                except (KeyError, AttributeError):
-                    handler = err
-                except Exception:
-                    handler = err
+                handler = self.error_controller(status_code=err.code)
+
                 try:
                     # try executing error_handler code
                     # otherwise re-raise the exception
                     return handler(environ, start_response)
                 except Exception:
+                    console.exception("Exception thrown during error_controller handling")
                     raise
             else:
+                # HTTPExceptions are also WSGI apps and can be called as such
                 return err(environ, start_response)
         except Exception, err:
             console.exception("General Exception thrown")
             if self.error_controller:
-                controller = self.error_controller()
-                handler = controller
-                controller.message = str(err)
-                return handler(environ, start_response)
+                handler = self.error_controller(message=str(err))
             else:
                 # create a generic HTTP server Error webob exception
-                return exc.HTTPServerError()(environ, start_response)
-
-
-def send_error_email(host=None, html=None, text=None):
-    if host:
-        host = ': {0}'.format(host)
-    else:
-        host = ''
-
-    me = "sysadmin@XXXX"
-    you = "ops@XXXX"
-    password = "XXXX"
-
-    msg = MIMEMultipart('alternative')
-    if text:
-        msg.attach(MIMEText(text, 'text'))
-
-    if html:
-        msg.attach(MIMEText(html, 'html'))
-
-    msg['Subject'] = '''{project_name} Exception'''.format(
-                                    project_name=project.project_name.title())
-    msg['From'] = "System Error{0} <{1}>".format(host, me)
-    msg['To'] = you
-
-    AuthUser=me
-    AuthPass=password
-
-    gmail = smtplib.SMTP('smtp.gmail.com',587)
-    gmail.ehlo()
-    gmail.starttls()
-    gmail.ehlo()
-    gmail.login(AuthUser, AuthPass)
-
-    gmail.sendmail(me, [you], msg.as_string())
-    gmail.close()
-
-    return True
+                handler = exc.HTTPServerError('General Fault')
+            return handler(environ, start_response)
 
 
 def pybald_error_template():
@@ -105,4 +71,5 @@ def pybald_error_template():
 
     This is targeted for deprecation.
     '''
+    console.warn("pybald_error_template is deprecated in pybald 0.2.0")
     return template_engine._get_template('stack_trace')
