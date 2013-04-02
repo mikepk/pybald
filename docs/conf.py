2ec0450ee0d91e828a10facf5f39a3438a1f519a
mikepk <mikepk@smarterer.com>
2013-04-02 16:06:18 -0400

    Update Copyright to 2013

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pybald'
-copyright = u'2012, Michael Kowalchik'
+copyright = u'2013, Michael Kowalchik'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
