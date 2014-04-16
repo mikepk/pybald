b0f3964417ff4c4684616310dcfbbc36bf17c5d1
mikepk <mikepk@smarterer.com>
2014-04-16 12:53:26 -0400

    Tweak the asset pipeline.

--- pybald/core/assets.py
+++ pybald/core/assets.py
@@ -10,48 +10,66 @@ from pybald.core import page
 import project
 import os
 from urlparse import urlparse
+
+try:
+    import pyhash
+    hashfunc = pyhash.super_fast_hash()
+except ImportError:
+    hashfunc = hash
+
 import logging
 console = logging.getLogger(__name__)
 
 env = Environment(os.path.join(project.path or '', "public"),
                   '',
                   debug=(not project.BUNDLE_ASSETS),
-                  auto_build=False)
-
-
-class AssetBundleSyntaxError(Exception):
-    pass
+                  auto_build=bool(project.BUNDLE_AUTO_BUILD))
 
 
 def _parse_bundle(elem, parent_bundle=None):
     '''Recursively generate webassets bundles by walking the xml tree'''
     if elem.tag != "bundle":
-        raise AssetBundleSyntaxError("Bundling only works with bundle tags.")
+        raise SyntaxError("Bundling only works with bundle tags.")
     contents = []
+    asset_type = None
+    asset_types = []
     # get all sub_bundles and process them
     for t in elem:
         if t.tag == 'bundle':
-            contents.append(_parse_bundle(t))
+            bundle, asset_type = _parse_bundle(t)
+            contents.append(bundle)
         elif t.tag == 'link':
             file_path = urlparse(t.attrib.get('href', '')).path
             contents.append(file_path.lstrip('/'))
+            asset_types.append('css')
         elif t.tag == 'script':
             file_path = urlparse(t.attrib.get('src', '')).path
             contents.append(file_path.lstrip('/'))
+            asset_types.append('js')
+
+    if not asset_types:
+        raise SyntaxError("No assets found in the bundle tag. Include javascript, css, or source files in the bundle.")
+    elif len(set(asset_types)) > 1:
+        raise SyntaxError("All assets in a bundle must be of the same type. They should all be javascript, css etc... or compiled to the same asset type.")
+    asset_type = asset_types[0]
 
     attrib_dict = dict(**elem.attrib)
-    attrib_dict.pop('asset_type', None)
     # convert text True/False to boolean
     if 'debug' in attrib_dict:
         if attrib_dict['debug'] == 'True':
             attrib_dict['debug'] = True
         elif attrib_dict['debug'] == 'False':
             attrib_dict['debug'] = False
+
+    # default temporary output file, just the version as name...
+    if 'output' not in attrib_dict:
+        attrib_dict['output'] = '/min/{0}/%(version)s.{0}'.format(asset_type)
+
     # remove leading slashes since webassets works on relative paths
     # a little hacky but keeps the document (web page) syntax cleaner
     attrib_dict['output'] = attrib_dict['output'].lstrip('/')
     new_bundle = Bundle(*contents, **attrib_dict)
-    return new_bundle
+    return new_bundle, asset_type
 
 
 class memoize_bundles(object):
@@ -64,9 +82,12 @@ class memoize_bundles(object):
         self.cached = {}
 
     def __call__(self, input_text):
-        if not self.cached.get(hash(input_text), None):
-            self.cached[hash(input_text)] = self.method(input_text)
-        return self.cached[hash(input_text)]
+        key = hashfunc(input_text)
+        try:
+            return self.cached[key]
+        except KeyError:
+            self.cached[key] = self.method(input_text)
+            return self.cached[key]
 
 
 @memoize_bundles
@@ -77,16 +98,15 @@ def bundle(input_text):
     '''
     # XML parse the fragments
     output_buffer = []
-    xml_template = u'''<fragments>{0}</fragments>'''
-    root = etree.fromstring(xml_template.format(input_text))
+    xml_fragment = u'''<fragments>{0}</fragments>'''.format(input_text)
+    root = etree.fromstring(xml_fragment)
     for elem in root:
         # parse all bundles
         if elem.tag == 'bundle':
             bundle_tag = elem
-            bundle = _parse_bundle(bundle_tag)
-            # every asset type has a default 'template'
-            asset_type = bundle_tag.attrib.get('asset_type', 'add_js')
-            link_func = getattr(page, asset_type)
+            bundle, asset_type = _parse_bundle(bundle_tag)
+            # every asset type has a default 'link type'
+            link_func = getattr(page, 'add_{0}'.format(asset_type))
             # construct the asset urls
             try:
                 assets = [link_func(url) for url in bundle.urls(env=env)]
