4a9c0f24c2ccee04cf4aaaf5667d4f39a09735bb
mikepk <mikepk@pluralsight.com>
2015-08-13 19:49:06 -0700

    Fix command line args on dev server

--- pybald/__init__.py
+++ pybald/__init__.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf-8
 # __init__.py
-#
-# Created by mikepk on 2009-07-24.
-# Copyright (c) 2009 Michael Kowalchik. All rights reserved.
+'''
+Pybald
+======
+
+An MVC-like framework using many best-of-breed components (i.e. webob, sqlalchemy).
+
+(c) 2015 Michael Kowalchik
+MIT License, see LICENSE file
+'''
 import imp
 import sys
 import os
