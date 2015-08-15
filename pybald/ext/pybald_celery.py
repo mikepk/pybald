6329b00b26b1b07363e6e4d65f25a98685bc710f
mikepk <mikepk@pluralsight.com>
2015-08-14 22:36:57 -0700

    Update pybald celery to use latest Celery

---
+++ pybald/ext/pybald_celery.py
@@ -0,0 +1,12 @@
+from celery import Celery
+
+def celery_config_item(item):
+    # check if the config item starts with any of the celery config start keys
+    celery_config_start_keys = ('CELERY', 'BROKER')
+    return any(item.startswith(key) for key in celery_config_start_keys)
+
+def make_celery(app):
+    celery = Celery(app.name)
+    celery.conf.update(dict((key, getattr(app.config, key)) for key in
+        filter(celery_config_item, dir(app.config))))
+    return celery
