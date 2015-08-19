81ecfcafe020778fb53662ada2c903b760531023
mikepk <mikepk@pluralsight.com>
2015-08-19 13:57:09 -0700

    Remove old-style project template

--- pybald/project_template/app/controllers/home_controller.py
+++
@@ -1,7 +0,0 @@
-from pybald.core.controllers import action, BaseController
-class HomeController(BaseController):
-    '''The Home page.'''
-
-    @action
-    def index(self, req):
-        return "Pybald is working."
--- pybald/project_template/environments/production.py
+++
@@ -1,6 +0,0 @@
-env_name="Production"
-debug=False
-green=False
-email_errors=True
-
-page_options= {'analytics' : True}
