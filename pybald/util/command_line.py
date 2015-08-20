5144696214c43c7681047de68cbff3ce2b32199c
mikepk <mikepk@pluralsight.com>
2015-08-19 20:35:30 -0700

    Add hack fix for python 3 for argparse
    Fix was found here:
        http://stackoverflow.com/questions/22990977/why-does-this-argparse-code-behave-differently-between-python-2-and-3

--- pybald/util/command_line.py
+++ pybald/util/command_line.py
@@ -3,13 +3,14 @@ from pybald.util.console import start_console
 from pybald.util.dev_server import start_dev_server
 def start(app):
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(help='sub-command help')
+    subparsers = parser.add_subparsers(help='sub-command help', dest="subcommand")
+    subparsers.required = True
     parser_console = subparsers.add_parser('console', help='console help')
+    parser_console.set_defaults(run=start_console)
     parser_serve = subparsers.add_parser('serve', help='serve help')
     parser_serve.add_argument('--host', type=str)
     parser_serve.add_argument('--port', type=int)
     parser_serve.set_defaults(host='0.0.0.0', port=8080)
-    parser_console.set_defaults(run=start_console)
     parser_serve.set_defaults(run=start_dev_server)
     options = parser.parse_args()
     options.run(app, options)
