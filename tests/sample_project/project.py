5619a50c415c1926e21c370c16150b2a0a3ba423
mikepk <mikepk@smarterer.com>
2015-08-24 17:59:54 -0400

    Comment out asset test
    Add asset test resources and config but disable for
    now until there's a way to cleanup the results.

--- tests/sample_project/project.py
+++ tests/sample_project/project.py
@@ -5,3 +5,7 @@ template_path = "tests/sample_project/"
 cache_path = None
 project_name = "Sample Project"
 debug = True
+template_helpers = ['from pybald.core import assets']
+BUNDLE_SOURCE_PATHS = ['tests/sample_project/front_end', 'tests/sample_project/sass']
+# BUNDLE_ASSETS = True
+path = ""
--- tests/unit/templates/test_assets.py
+++ tests/unit/templates/test_assets.py
@@ -7,13 +7,13 @@ class TestWebassets(unittest.TestCase):
         context = pybald.configure(config_file="tests/sample_project/project.py")
         from tests.sample_project.sample import app
 
-    def test_bundles(self):
-        '''Run webasset bundling'''
-        from pybald import context
-        template = context.render._get_template('sample_with_bundles')
-        data = template.render()
-        print(data)
-        assert False
-        # we've got a compiled template
-        # assert isinstance(template, Template)
+    # def test_bundles(self):
+    #     '''Run webasset bundling'''
+    #     from pybald import context
+    #     template = context.render._get_template('sample_with_bundles')
+    #     data = template.render()
+    #     print(data)
+    #     assert False
+    #     # we've got a compiled template
+    #     # assert isinstance(template, Template)
 
