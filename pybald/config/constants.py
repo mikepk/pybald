f0fdb6769d47718b1f5c82d3885606ed466a9d4e
mikepk <mikepk@pluralsight.com>
2017-06-12 14:10:54 -0600

    Add docstring to constants module and read func

--- pybald/config/constants.py
+++ pybald/config/constants.py
@@ -1,3 +1,9 @@
+'''The constants helper module is to allow for creating 'safer' ini files for
+values that will be injected into templates. The basic rationale is that there
+are often values that you want non-technical people to control (phone numbers,
+marketing claims, addresses) and similar values. Using constants allows these
+individuals to use friendlier ini format but still have the values available
+for page templates.'''
 try:
     from ConfigParser import ConfigParser as configparser
 except ImportError:
@@ -12,6 +18,12 @@ from collections import namedtuple
 
 
 def read(filename='constants.ini', constants_file=None):
+    '''Read a constants ini file and return the constants helper object.
+
+    When added to the page_options config, these constants can be used inside
+    the templates where the section/data of the constants ini is dereferenced
+    using constants.SECTION.KEY.
+    '''
     config = configparser()
     if constants_file:
         config.readfp(constants_file)
