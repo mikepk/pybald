24bf98117fce6baed237e8e67a59e64a9540cedf
mikepk <mikepk@pluralsight.com>
2017-06-12 12:32:20 -0600

    Cleanup spacing PEP8

--- pybald/ext/pybald_celery.py
+++ pybald/ext/pybald_celery.py
@@ -1,9 +1,11 @@
 from celery import Celery
 
+
 def celery_config_item(item):
     # check if the config item starts with any of the celery config start keys
     celery_config_start_keys = ('CELERY', 'BROKER')
     return any(item.startswith(key) for key in celery_config_start_keys)
+
 
 def make_celery(app):
     celery = Celery(app.name)
