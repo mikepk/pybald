f90f848d723118a696be3418e8d8ceca510d374f
David Sturgis <david-sturgis@pluralsight.com>
2019-02-14 12:03:24 -0500

    Update __init__.py
    Update __version__ for 0.5.3 release

--- pybald/__init__.py
+++ pybald/__init__.py
@@ -20,7 +20,7 @@ from .default import default_config
 from collections import namedtuple
 log = logging.getLogger(__name__)
 
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 
 
 def read_config_module(root_path='.', filename='project.py'):
